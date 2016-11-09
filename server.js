106d69231a446a5e63421faba77c3a7be3351df3
Kliment Trajcheski <kliment.trajceski@gmail.com>
2016-11-09 19:39:58 +0100

    Fixing http

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ var app = express();
 
 const PORT = process.env.PORT||3000;
 
+app.use(function(req, res, next){
+    if(req.headers['x-forwarded-proto'] === 'http'){
+        next();
+    }else{
+        res.redirect('http://'+req.hostname + req.url);
+    }
+});
+
 app.use(express.static('public'));
 
 app.listen(PORT, function(){
